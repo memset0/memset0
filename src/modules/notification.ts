4db3d4a2c8384a9891829b465cc1af07795b0a7b
memset0 <34177126+memset0@users.noreply.github.com>
2021-09-06 23:10:20 +0800

    refactor: notification

--- src/modules/notification.ts
+++ src/modules/notification.ts
@@ -2,5 +2,5 @@ import { join } from 'path';
 import { readFileSync } from "fs";
 
 export default function () {
-	return readFileSync(join(__dirname, '../../data/notification.html')).toString();
+	return readFileSync(join(__dirname, '../../pages/notification.md')).toString();
 }

c84bee3861f5f9e19b0b092cd488168dd1ad0dc0
memset0 <34177126+memset0@users.noreply.github.com>
2021-09-08 00:44:12 +0800

    feat: add link to statistics page

--- src/modules/tag-cloud.ts
+++ src/modules/tag-cloud.ts
@@ -2,9 +2,17 @@ import * as path from 'path';
 import * as md5File from 'md5-file';
 import { assetLink } from '../utils';
 
+const image_link = assetLink('/tagcloud.png');
+const statistics_link = 'https://github.com/memset0/memset0/blob/master/pages/tags.md';
+
 export default function () {
 	const h = md5File.sync(path.join(__dirname, '../../assets/tagcloud.png'));
 	console.log('[tag-cloud]', 'md5-hash', h);
 
-	return `<p align="center"><img src="${assetLink(`/tagcloud.png?h=${h.slice(0, 6)}&c=${Date.now()}`)}" height="150"></p>`
+	return '<p align="center"><a href="' +
+		statistics_link +
+		'"><img src="' +
+		image_link +
+		`?h=${h.slice(0, 6)}&c=${Date.now()}` +
+		'" height="150"></a></p>';
 }

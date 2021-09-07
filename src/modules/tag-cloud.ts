54ac9cd59e8d65637cd64abc9e0cefbeb26d32ed
memset0 <34177126+memset0@users.noreply.github.com>
2021-09-08 00:26:42 +0800

    fix: typo

--- src/modules/tag-cloud.ts
+++ src/modules/tag-cloud.ts
@@ -6,5 +6,5 @@ export default function () {
 	const h = md5File.sync(path.join(__dirname, '../../assets/tagcloud.png'));
 	console.log('[tag-cloud]', 'md5-hash', h);
 
-	return `<p align="center"><img src="${assetLink(`/tagclound.png?h=${h.slice(0, 6)}&c=${Date.now()}`)}" height="150"></p>`
+	return `<p align="center"><img src="${assetLink(`/tagcloud.png?h=${h.slice(0, 6)}&c=${Date.now()}`)}" height="150"></p>`
 }

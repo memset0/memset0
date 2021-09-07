103c71f2dee6784f10a594e0365f5c2a0c349221
memset0 <34177126+memset0@users.noreply.github.com>
2021-09-08 00:23:49 +0800

    fix: modules not import

--- src/modules/tag-cloud.ts
+++ src/modules/tag-cloud.ts
@@ -1,9 +1,10 @@
+import * as path from 'path';
 import * as md5File from 'md5-file';
 import { assetLink } from '../utils';
 
 export default function () {
 	const h = md5File.sync(path.join(__dirname, '../../assets/tagcloud.png'));
 	console.log('[tag-cloud]', 'md5-hash', h);
-	
-	return `<p align="center"><img src="${assetLink('/tagclound.png?h=' + h)}" height="150"></p>`
+
+	return `<p align="center"><img src="${assetLink(`/tagclound.png?h=${h}&c=${Date.now()}`)}" height="150"></p>`
 }

26710404ab54a994ca347bf9b53e1b4b788d7edc
whisperain‮ <34177126+memset0@users.noreply.github.com>
2021-09-11 20:06:07 +0800

    upd

--- src/modules/activity.ts
+++ src/modules/activity.ts
@@ -2,7 +2,7 @@ import { load } from 'cheerio';
 import { get, agent } from 'superagent';
 import { assetLink, generateTable, TableCell } from '../utils';
 
-const max_length = 6;
+const max_length = 8;
 
 const chineseNumbers = [
 	'一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二'

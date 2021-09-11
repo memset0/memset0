f3927105b87520a1874f63490884ae606e2719d0
memset0 <34177126+memset0@users.noreply.github.com>
2021-09-11 16:11:31 +0800

    fix: uri quirks of 163music

--- src/modules/activity.ts
+++ src/modules/activity.ts
@@ -130,9 +130,9 @@ async function crawlFavoriteMusic() {
 			console.log('[crawl-favorite-music]', id, name, artist, album);
 
 			return '* ' +
-				`<a href="https://music.163.com/song?id=${id}"><strong>${name}</strong></a> ` +
+				`<a href="https://music.163.com/#/song?id=${id}"><strong>${name}</strong></a> ` +
 				(artist ? `- ${artist} ` : '') +
-				(album && album_id ? `- <a href="https://music.163.com/album?id=${album_id}">${album}</a> ` : '');
+				(album && album_id ? `- <a href="https://music.163.com/#/album?id=${album_id}">${album}</a> ` : '');
 		}).join('\n');
 }
 
